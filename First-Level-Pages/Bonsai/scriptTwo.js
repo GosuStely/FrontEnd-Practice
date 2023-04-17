dd6e283b7cc150e44e5de34e0ceb8c6875af80ac
Stelian Tishelov <stely.tishelov@gmail.com>
2023-04-17 21:41:54 +0300

    s

--- First-Level-Pages/Bonsai/scriptTwo.js
+++ First-Level-Pages/Bonsai/scriptTwo.js
@@ -1,18 +1,16 @@
 const toggleElement = document.getElementById('toggle');
-const workflowElement = document.getElementById('workflow-price')
-const workflowPlusElement = document.getElementById('workflow-plus-price')
+const workflowElement = document.getElementById('workflow-price');
+const workflowPlusElement = document.getElementById('workflow-plus-price');
 toggleElement.checked = true;
-toggleElement.addEventListener("change", (e) =>
-{
-if(toggleElement.checked == true){
-workflowElement.innerHTML ="203";
-workflowPlusElement.innerHTML="301";
-toggleElement.checked = false;
-}
-else{
-    workflowElement.innerHTML ="19";
-    workflowPlusElement.innerHTML="29";
-    console.log('lol')
+toggleElement.addEventListener('change', e => {
+  if (toggleElement.checked == true) {
+    workflowElement.innerHTML = '203';
+    workflowPlusElement.innerHTML = '301';
+    toggleElement.checked = false;
+  } else {
+    workflowElement.innerHTML = '19';
+    workflowPlusElement.innerHTML = '29';
+    console.log('lol');
     toggleElement.checked = true;
-}
-})
+  }
+});
