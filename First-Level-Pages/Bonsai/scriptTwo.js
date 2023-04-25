96687fcc63b7397539fdc024f85ff2f0cd0a5042
Stelian Tishelov <stely.tishelov@gmail.com>
2023-04-25 12:46:15 +0300

    price fix

--- First-Level-Pages/Bonsai/scriptTwo.js
+++ First-Level-Pages/Bonsai/scriptTwo.js
@@ -1,15 +1,23 @@
 const toggleElement = document.getElementById('toggle');
 const workflowElement = document.getElementById('workflow-price');
 const workflowPlusElement = document.getElementById('workflow-plus-price');
+
+const priceTagElement = document.getElementById('normal-price-tag');
+const plusPriceTagElement = document.getElementById('plus-price-tag');
 toggleElement.checked = true;
+
 toggleElement.addEventListener('change', e => {
   if (toggleElement.checked == true) {
     workflowElement.innerHTML = '203';
     workflowPlusElement.innerHTML = '301';
+    priceTagElement.innerHTML = `$ <span>${workflowElement.innerHTML}</span> /YEAR`;
+    plusPriceTagElement.innerHTML = `$ <span>${workflowPlusElement.innerHTML}</span> /YEAR`;
     toggleElement.checked = false;
   } else {
     workflowElement.innerHTML = '19';
     workflowPlusElement.innerHTML = '29';
+    priceTagElement.innerHTML = `$ <span>${workflowElement.innerHTML}</span> /MONTH`;
+    plusPriceTagElement.innerHTML = `$ <span>${workflowPlusElement.innerHTML}</span> /MONTH`;
     console.log('lol');
     toggleElement.checked = true;
   }
